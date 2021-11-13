67eebbb82508d16da6e3beb90b6bfbfa842f2f2a
Bodhish Thomas <bodhish@gmail.com>
2021-11-13 22:16:07 +0530

    Extend colors

--- package/tailwind.config.js
+++ package/tailwind.config.js
@@ -5,9 +5,10 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: colors.indigo,
+    extend: {
+      colors: {
+        primary: colors.indigo,
+      },
     },
   },
   variants: {
