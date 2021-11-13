e667b165f533c637f4604cf770957951600da549
vinu tv <vinutv91@gmail.com>
2021-11-14 03:13:44 +0530

    Add Inter font

--- package/components/meta.js
+++ package/components/meta.js
@@ -6,6 +6,12 @@ export default function Meta() {
       <meta name="description" content={`Knowledge Base Center.`} />
       <link rel="icon" href="/favicon.ico" />
       <title>Knowledgebase</title>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700;800&family=Roboto+Mono&display=swap"
+        rel="stylesheet"
+      />
     </Head>
   );
 }
