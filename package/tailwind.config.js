de48cc8047b155c805f41f3978858e7bf968e446
vinu tv <vinutv91@gmail.com>
2021-11-14 03:11:27 +0530

    Add custom css and update colors

--- package/tailwind.config.js
+++ package/tailwind.config.js
@@ -5,9 +5,22 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: colors.indigo,
+    extend: {
+      fontFamily: {
+        inter:
+          'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";',
+      },
+      colors: {
+        primary: colors.indigo,
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        gray: colors.coolGray,
+        indigo: colors.indigo,
+        red: colors.rose,
+        yellow: colors.amber,
+      },
     },
   },
   variants: {
